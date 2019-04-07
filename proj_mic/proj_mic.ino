604ceaf16266411ccc5c11e1e1bb36fbd9795722
Piotr Klimaszewski <pixelarious@gmail.com>
2019-04-07 23:43:13 +0200

    added kinda working letter translator

--- proj_mic/proj_mic.ino
+++ proj_mic/proj_mic.ino
@@ -1,5 +1,17 @@
 static const int MIC_PIN = A0;
 
+const char *englishAlphabet[] = {"A", "B", "C", "D", "E", "F", "G", 
+                          "H", "I", "J", "K", "L", "M", "N",
+                          "O", "P", "Q", "R", "S", "T", "U", 
+                          "V", "W", "X", "Y", "Z"};
+                                
+const char *morseCodes[] = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.",
+                            "....", "..", ".---.", "-.-", ".-..", "--", "-.",
+                            "---", ".--.", "--.-", ".-.", "...", "-", "..-", "..._",
+                            ".--", "-..-", "-.--", "--.."};
+
+char msg[32];
+
 float bufor[20];
 float x = 0;
 float y = 0;
@@ -51,31 +63,41 @@ void loop() {
       t_01 = micros();
       dt = t_01 - t_10;
       if (dt > 1000000) {
-        Serial.println('|');
+        //Serial.println('|');
+        Serial.println(msg);
+        MorseCodeToEnglishAlphabet(msg);
+        strcpy(msg, "");
       }
     } else if (discrLast == 1 and discr ==0) { // if from 1 to 0
       t_10 = micros();
       dt = t_10 - t_01;
       if (dt > 200000 and dt < 1000000) {
-          Serial.print('-');
+          //Serial.print('-');
+          strcat(msg, "-");
       } else if (dt <= 200000 and dt > 50000) {
-          Serial.print('.');
+          //Serial.print('.');
+          strcat(msg, ".");
       }   
     }
   } 
+
   
-//  Serial.print(x);
-//  Serial.print(',');
-//  Serial.print(avg);
-//  Serial.print(',');
-//  Serial.println(temp*300);
 
   discrLast = discr;
   delay(1);
 }
 
 // LowPassFilter()
-
 float LowPassFilter(float input, float output, float alpha) {
   return output + alpha * (input - output);
+}
+
+void MorseCodeToEnglishAlphabet(char m[]) {
+  int alphabetLength = 27;
+  for (int i = 0; i < alphabetLength - 1; i++) {
+    if (strcmp(morseCodes[i], m) == 0) {
+      strcat(m, englishAlphabet[i]);
+    }
+  }
+  Serial.println(m);
 }
