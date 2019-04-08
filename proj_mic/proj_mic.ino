a5d818df28e5e581e20be29acf974fd8d4c49a72
Piotr Klimaszewski <pixelarious@gmail.com>
2019-04-08 23:12:32 +0200

    making it clearer

--- proj_mic/proj_mic.ino
+++ proj_mic/proj_mic.ino
@@ -1,21 +1,23 @@
+/* PINS */
 static const int MIC_PIN = A0;
 
-const char *englishAlphabet[] = {"A", "B", "C", "D", "E", "F", "G", 
-                          "H", "I", "J", "K", "L", "M", "N",
-                          "O", "P", "Q", "R", "S", "T", "U", 
-                          "V", "W", "X", "Y", "Z"};
-                                
-const char *morseCodes[] = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.",
-                            "....", "..", ".---.", "-.-", ".-..", "--", "-.",
-                            "---", ".--.", "--.-", ".-.", "...", "-", "..-", "..._",
-                            ".--", "-..-", "-.--", "--.."};
-
+/* Arrays and variables for translating Morse Code*/
+const char *englishAlphabet[] = {"A", "B", "C", "D", "E", "F", "G",
+                                 "H", "I", "J", "K", "L", "M", "N",
+                                 "O", "P", "Q", "R", "S", "T", "U",
+                                 "V", "W", "X", "Y", "Z"
+                                };
+const char *morseCode[] = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.",
+                           "....", "..", ".---.", "-.-", ".-..", "--", "-.",
+                           "---", ".--.", "--.-", ".-.", "...", "-", "..-", "..._",
+                           ".--", "-..-", "-.--", "--.."
+                          };
 char msg[32];
 
 float bufor[20];
-float x = 0;
-float y = 0;
-float h = 0;
+float inputFromMic = 0;
+float afterLPF = 0;
+float afterHPF = 0;
 
 double t_01;
 double t_10;
@@ -26,20 +28,22 @@ int avg = 0;
 int discr = 0;
 int discrLast = 0;
 
-void setup() { 
+void setup() {
   pinMode(MIC_PIN, INPUT);
   Serial.begin(115200);
 }
 
 void loop() {
-  x = analogRead(MIC_PIN);  // input
-  y = LowPassFilter(x, y, 0.7); // after LPF
-  
-  h = x-y;  // "after" HPF
-  h = 2 * sqrt(h*h);
+  inputFromMic = analogRead(MIC_PIN);
 
-  // Moving average
-  bufor[n] = h;
+  /* Passing signal through a High-Pass Filter
+    (by substracting a Low-Pass Filter output from unfiltered signal) */
+  afterLPF = lowPassFilter(inputFromMic, afterLPF, 0.7);
+  afterHPF = inputFromMic - afterLPF;
+  afterHPF = 2 * sqrt(afterHPF * afterHPF);
+
+  /* Moving average */
+  bufor[n] =  afterHPF ;
   n++;
   if (n >= 19) {
     n = 0;
@@ -52,7 +56,7 @@ void loop() {
 
   // to {1, 0}
   if (avg > 50) {
-    discr = 1; 
+    discr = 1;
   } else {
     discr = 0;
   }
@@ -64,40 +68,42 @@ void loop() {
       dt = t_01 - t_10;
       if (dt > 1000000) {
         //Serial.println('|');
-        Serial.println(msg);
-        MorseCodeToEnglishAlphabet(msg);
-        strcpy(msg, "");
+        //Serial.println(msg);
+        morseCodeToEnglishAlphabet(msg);
+        strcpy( msg, "" );
       }
-    } else if (discrLast == 1 and discr ==0) { // if from 1 to 0
+    } else if (discrLast == 1 and discr == 0) { // if from 1 to 0
       t_10 = micros();
       dt = t_10 - t_01;
       if (dt > 200000 and dt < 1000000) {
-          //Serial.print('-');
-          strcat(msg, "-");
+        //Serial.print('-');
+        strcat(msg, "-");
       } else if (dt <= 200000 and dt > 50000) {
-          //Serial.print('.');
-          strcat(msg, ".");
-      }   
+        //Serial.print('.');
+        strcat(msg, ".");
+      }
     }
-  } 
+  }
 
-  
+  Serial.print(inputFromMic);
+  Serial.print(',');
+  Serial.println(afterHPF);
 
   discrLast = discr;
   delay(1);
 }
 
 // LowPassFilter()
-float LowPassFilter(float input, float output, float alpha) {
-  return output + alpha * (input - output);
+float lowPassFilter(float x, float y, float a) {
+  return y + a * (x - y);
 }
 
-void MorseCodeToEnglishAlphabet(char m[]) {
+void morseCodeToEnglishAlphabet(char m[]) {
   int alphabetLength = 27;
   for (int i = 0; i < alphabetLength - 1; i++) {
-    if (strcmp(morseCodes[i], m) == 0) {
+    if (strcmp(morseCode[i], m) == 0) {
       strcat(m, englishAlphabet[i]);
     }
   }
-  Serial.println(m);
+  //Serial.println(m);
 }
