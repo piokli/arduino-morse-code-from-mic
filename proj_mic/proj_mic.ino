1d3c8287e3d332a1d27c864383ca50175de921ad
piokli <49364948+piokli@users.noreply.github.com>
2019-05-16 09:23:28 +0200

    Update proj_mic.ino

--- proj_mic/proj_mic.ino
+++ proj_mic/proj_mic.ino
@@ -1,32 +1,58 @@
-/* PINS */
+/*
+      Morse Code to English Alphabet
+
+      In a few words this sketch allows to interpret buzzer's "beeps" as morse code and
+      then translate them to english alphabet.
+
+      The circuit:
+      * components: buzzer, electrolyt microphone, button, 1k resistor
+      * circuit template will be attached as an .png file
+      
+      Last modified 06.05.2019
+*/
+
+/* PINs */
 static const int MIC_PIN = A0;
 
-/* Arrays and variables for translating Morse Code*/
+/* Arrays and bufor for translating morse code */
 const char *englishAlphabet[] = {"A", "B", "C", "D", "E", "F", "G",
                                  "H", "I", "J", "K", "L", "M", "N",
                                  "O", "P", "Q", "R", "S", "T", "U",
-                                 "V", "W", "X", "Y", "Z"
+                                 "V", "W", "X", "Y", "Z",
+                                 "1", "2", "3", "4", "5",
+                                 "6", "7", "8", "9", "0"
                                 };
 const char *morseCode[] = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.",
                            "....", "..", ".---.", "-.-", ".-..", "--", "-.",
-                           "---", ".--.", "--.-", ".-.", "...", "-", "..-", "..._",
-                           ".--", "-..-", "-.--", "--.."
+                           "---", ".--.", "--.-", ".-.", "...", "-", "..-",
+                           "..._", ".--", "-..-", "-.--", "--..",
+                           ".----", "..---", "...--", "....-", ".....",
+                           "-....", "--...", "---..", "----.", "-----"
                           };
 char msg[32];
 
-float bufor[20];
-float inputFromMic = 0;
-float afterLPF = 0;
-float afterHPF = 0;
+/* Variables (vectors as for [n, n-1] means [currentProbe, lastProbe])
+   for getting data acquisition and filtering */
+const int n = 1;
+float inputFromMic[] = {0, 0};
+float filterOutput[] = {0, 0};
+float absoluteValue[] = {0, 0};
+int discr[] = {0, 0};
+float avg = 0;
 
+const int wielkoscBufora=20;
+float bufor[wielkoscBufora];
+int k = 0;
+
+/* Used for measuring beep's time length */
 double t_01;
 double t_10;
-double dt;
 
-int n = 0;
-int avg = 0;
-int discr = 0;
-int discrLast = 0;
+
+
+/* M A I N */
+
+
 
 void setup() {
   pinMode(MIC_PIN, INPUT);
@@ -34,76 +60,93 @@ void setup() {
 }
 
 void loop() {
-  inputFromMic = analogRead(MIC_PIN);
-
-  /* Passing signal through a High-Pass Filter
-    (by substracting a Low-Pass Filter output from unfiltered signal) */
-  afterLPF = lowPassFilter(inputFromMic, afterLPF, 0.7);
-  afterHPF = inputFromMic - afterLPF;
-  afterHPF = 2 * sqrt(afterHPF * afterHPF);
-
-  /* Moving average */
-  bufor[n] =  afterHPF ;
-  n++;
-  if (n >= 19) {
-    n = 0;
+  /* Acquiring data from microphone */
+  inputFromMic[n-1] = inputFromMic[n];
+  inputFromMic[n] = analogRead(MIC_PIN);
+  /* Passing through high pass filter */
+  filterOutput[n-1] = filterOutput[n];
+  filterOutput[n] = highPassFilter(inputFromMic, filterOutput, 0.000145, 2000);
+  /* Getting absolute */
+  absoluteValue[n-1] = abs(filterOutput[n-1]);
+  absoluteValue[n] = abs(filterOutput[n]);
+  /* Filling the bufor */
+  bufor[k] = absoluteValue[n];
+  k++;
+  if (k >= wielkoscBufora) {
+    k = 0;
   }
+  /* Averaging signal */
   avg = 0;
-  for (int i = 0; i < 20; i++) {
-    avg = avg + bufor[i];
+  for (int i = 0; i < wielkoscBufora; i++) {
+    avg += bufor[i];
   }
-  avg = avg / 20;
-
-  // to {1, 0}
-  if (avg > 50) {
-    discr = 1;
+  /* Filtered analog signal to discrete (0 and 1) */
+  discr[n-1] = discr[n];
+  if (avg > 800) {
+    discr[n] = 1;
   } else {
-    discr = 0;
+    discr[n] = 0;
   }
+  
+  discrToMorse(discr);
+}
 
-  // Signal to '.' and '_'
-  if (discrLast != discr) {
-    if (discrLast == 0 and discr == 1) { // if from 0 to 1
+
+
+/* High Pass Filter function gets ... */
+float highPassFilter(float x[], float y[], float dt, float fc) {
+  float a = 1/(2*3.14*dt*fc + 1);
+  return y[n] = a * (y[n-1] + x[n] - x[n-1]);
+}
+/* blablaaaa */
+
+
+float lowPassFilter(float x[], float y[], float a) {
+  return y[n] = y[n-1] + a * (x[n] - y[n-1]);
+}
+
+
+/* blabla... */
+/* przerwa miedzy literami > 1s
+   '-' od 200ms do 1s 
+   '.' od 50ms do 200ms*/
+void discrToMorse(int d[2])
+{
+  // najpierw wykrywamy zmianę z 0 na 1 i na odwrót
+  if (d[n-1] != d[n]) {
+    double dt;
+    if (d[n-1] == 0 and d[n] == 1) {
+      // zmiana 0 -> 1
       t_01 = micros();
       dt = t_01 - t_10;
       if (dt > 1000000) {
         //Serial.println('|');
-        //Serial.println(msg);
+        //Serial.print(msg);
         morseCodeToEnglishAlphabet(msg);
-        strcpy( msg, "" );
+        Serial.println(msg);
+        strcpy(msg, "");
       }
-    } else if (discrLast == 1 and discr == 0) { // if from 1 to 0
-      t_10 = micros();
-      dt = t_10 - t_01;
-      if (dt > 200000 and dt < 1000000) {
-        //Serial.print('-');
-        strcat(msg, "-");
-      } else if (dt <= 200000 and dt > 50000) {
-        //Serial.print('.');
-        strcat(msg, ".");
-      }
+    } else if(d[n-1] == 1 and d[n] == 0) {
+      // zmiana 1 -> 0
+       t_10 = micros();
+       dt = t_10 - t_01;
+       if (dt > 200000 and dt < 1000000) {
+         //Serial.print('-');
+         strcat(msg, "-");
+       } else if (dt <= 200000 and dt > 50000) {
+         //Serial.print('.');
+         strcat(msg, ".");
+       }
     }
   }
-
-  Serial.print(inputFromMic);
-  Serial.print(',');
-  Serial.println(afterHPF);
-
-  discrLast = discr;
-  delay(1);
 }
 
-// LowPassFilter()
-float lowPassFilter(float x, float y, float a) {
-  return y + a * (x - y);
-}
 
 void morseCodeToEnglishAlphabet(char m[]) {
-  int alphabetLength = 27;
+  int alphabetLength = 37;
   for (int i = 0; i < alphabetLength - 1; i++) {
     if (strcmp(morseCode[i], m) == 0) {
-      strcat(m, englishAlphabet[i]);
+      strcpy(m, englishAlphabet[i]);
     }
   }
-  //Serial.println(m);
 }
